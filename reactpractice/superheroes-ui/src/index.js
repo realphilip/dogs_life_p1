760dd0292245c538088c12a007a60afb43a11e53
Mario <marioirimia@gmail.com>
2023-07-31 17:43:53 +0300

    QL3 React Static Version
    Finished QL3 React Static

--- reactpractice/superheroes-ui/src/index.js
+++ reactpractice/superheroes-ui/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Batman from "./images/1.jpeg";
+import Superman from "./images/2.jpeg";
+import GreenLantern from "./images/4.jpeg";
+import TheHuntress from "./images/5.jpeg";
+import TheFlash from "./images/3.jpeg";
+import HarleyQuinn from "./images/6.jpeg";
+import Deadshot from "./images/7.jpeg";
+import CountVertigo from "./images/8.jpeg";
+import Robin from "./images/9.jpeg";
+import Nightwing from "./images/10.jpeg";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +20,20 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const images = {
+  Batman,
+  Superman,
+  GreenLantern,
+  TheHuntress,
+  TheFlash,
+  HarleyQuinn,
+  Deadshot,
+  CountVertigo,
+  Robin,
+  Nightwing
+}
+export default images;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
