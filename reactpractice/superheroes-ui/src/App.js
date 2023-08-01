9650cf1d1fa6ecf1f730e41087e7ac84e97c3cd6
Mario <marioirimia@gmail.com>
2023-08-01 11:20:13 +0300

    QL4 Working

--- reactpractice/superheroes-ui/src/App.js
+++ reactpractice/superheroes-ui/src/App.js
@@ -3,30 +3,35 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Routes  from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AllHeroes from './components/AllHeroes';
+import HeroForm from './components/HeroForm';
+
 
 function App() {
   return (
     <>
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-      <Navbar.Brand href="/">Heroes</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <Nav.Link href="/allheroes">All Heroes</Nav.Link>
-          <Nav.Link href="/addhero">Add Hero</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    
-    </Navbar>
-    <Routes>
-      <Route path="/allheroes" element={AllHeroes} />
-      <Route path="/addhero" element={HeroForm} />
-      <Route path="/" element={AllHeroes} />
-    </Routes>
-</>
+  
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="/">Heroes</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/allheroes">All Heroes</Nav.Link>
+              <Nav.Link href="/addhero">Add Hero</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      
+        <Routes>
+          <Route path="allheroes" element={<AllHeroes/>} />
+          <Route path="addhero" element={<HeroForm/>} />
+          <Route path="/" element={<AllHeroes/>} />
+        </Routes>
+      
+   </> 
   );
 }
 
