6bf4113e4bfdc6bd1a4a2691a80588514ddcb8ee
Mario <marioirimia@gmail.com>
2023-08-01 17:42:09 +0300

    Update DogController Origin Cross

--- java-api/src/main/java/com/db/grad/javaapi/controller/DogsController.java
+++ java-api/src/main/java/com/db/grad/javaapi/controller/DogsController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class DogsController {
   @Autowired
   DogService dogService;
